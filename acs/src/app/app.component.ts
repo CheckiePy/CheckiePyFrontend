d3a220acaf9d16759b5c74444f90a343b50ffdcc
Artem Ustimov <art@ustimov.org>
2017-06-26 01:11:37 +0300

    Web service drafts

--- acs/src/app/app.component.ts
+++ acs/src/app/app.component.ts
@@ -1,18 +1,32 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {WebService} from './web.service';
+import {Router, NavigationEnd} from "@angular/router";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-    title = 'app works!';
+export class AppComponent implements OnInit {
+
     isAuthenticated = false;
 
-    constructor(private _webService: WebService) {
-        if (_webService.isAuthenticated()) {
-            this.isAuthenticated = true;
-        }
+    constructor(private _router: Router, private _webService: WebService) {
+    }
+
+    ngOnInit() {
+        this._router.events.subscribe(event => {
+            if (event instanceof NavigationEnd) {
+                let urlTree = this._router.parseUrl(this._router.url);
+                let token = urlTree.queryParams['token'];
+                console.log('Token: ' + token);
+                if (token != null) {
+                    this._webService.setToken(token);
+                    console.log('Token was set');
+                }
+                this.isAuthenticated = this._webService.isAuthenticated();
+                this._router.navigate(['']);
+            }
+        });
     }
 }
--- acs/src/app/web.service.ts
+++ acs/src/app/web.service.ts
@@ -1,12 +1,41 @@
 import {Injectable} from '@angular/core';
+import {RequestOptions, Http, Headers} from "@angular/http";
 
 @Injectable()
 export class WebService {
 
-    constructor() {
+    private _baseUrl = 'http://127.0.0.1:8000/api';
+    private _token: string;
+    private _options: RequestOptions;
+
+    constructor(private _http: Http) {
+    }
+
+    private updateOptions() {
+        let headers = new Headers({
+           'Content-Type': 'application/json'
+        });
+        if (this.isAuthenticated()) {
+            headers.append('Authorization', 'Token: ' + this._token);
+        }
+        this._options = new RequestOptions({
+            headers: headers,
+            withCredentials: true
+        });
     }
 
     isAuthenticated(): boolean {
-        return true;
+        return this._token != null;
+    }
+
+    setToken(token: string) {
+        this._token = token;
+        this.updateOptions();
+    }
+
+    getRepositoryList() {
+        this._http.get(this._baseUrl + '/repository/list/', this._options).subscribe(response => {
+            console.log(response);
+        });
     }
 }
